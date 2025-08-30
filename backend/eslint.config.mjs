07a703837b927b2b75a916f6b89f7212970a0c57
joseDanielRestrepoOrozco <jose.resoro4789@gmail.com>
2025-08-29 22:10:58 -0500

    first commit

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,33 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default tseslint.config({
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked
+  ],
+  plugins: {
+    js: eslint,
+    '@typescript-eslint': tseslint.plugin,
+    '@stylistic': stylistic
+  },
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname
+    }
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@stylistic/quotes': ['error', 'single'],
+    '@stylistic/semi': ['error', 'always'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/restrict-plus-operands': 'off',
+    '@typescript-eslint/no-case-declarations': 'off'
+  },
+  ignores: ['build/**/*', 'eslint.config.mjs']
+})
